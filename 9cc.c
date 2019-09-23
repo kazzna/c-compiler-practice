8a32849227ee8c0013d7ac0549a4323e40fa2f3f
Kazunari Mori <hykathon@gmail.com>
2019-09-23 14:24:45 +0900

    単項プラスと単項マイナス

--- 9cc.c
+++ 9cc.c
@@ -9,6 +9,7 @@ typedef struct Node Node;
 
 Node *expr(void);
 Node *mul(void);
+Node *unary(void);
 Node *primary(void);
 
 typedef enum {
@@ -185,16 +186,26 @@ Node *expr() {
 
 // Parse mul
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 
 	for (;;) {
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
+}
+
+// Parse unary
+Node *unary() {
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
+	
 }
 
 // Parse primary
